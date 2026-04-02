0de9035af1c530b8b8409f561994408c9ec92ba1
Harshad Reddy Nalla <hnalla@redhat.com>
2026-04-02 02:14:01 -0400

    Added static file server that serves frontend assets of notebooksV2
    Signed-off-by: Harshad Reddy Nalla <hnalla@redhat.com>

--- packages/notebooks/upstream/workspaces/backend/cmd/main.go
+++ packages/notebooks/upstream/workspaces/backend/cmd/main.go
@@ -130,6 +130,14 @@ func main() {
 		openapi.SwaggerInfo.BasePath = cfg.SwaggerBasePath
 		openapi.SwaggerInfo.Schemes = []string{cfg.SwaggerScheme}
 	}
+	flag.StringVar(
+		&cfg.StaticAssetsDir,
+		"static-assets-dir",
+		getEnvAsStr("STATIC_ASSETS_DIR", "/static"),
+		"Directory containing frontend static assets",
+	)
+
+	flag.Parse()
 
 	// Initialize the logger
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
